952a322e9ea55463e4421e6e110323a924aaf6bf
Parth Gajjar <gajjar.parth1811@gmail.com>
2022-10-13 21:40:09 +0530

    [WIP]: Improve command

--- src/Console/Commands/InstallCommand.php
+++ src/Console/Commands/InstallCommand.php
@@ -2,7 +2,9 @@
 
 namespace Parth1895\LxBladeComponents\Console\Commands;
 
+use RuntimeException;
 use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
 use Symfony\Component\Process\Process;
 
 class InstallCommand extends Command
@@ -30,11 +32,29 @@ class InstallCommand extends Command
     {
         // $this->callSilent('vendor:publish', ['--tag' => 'lx-views', '--force' => true]);
         // $this->output->writeln(PHP_EOL.'<bg=blue;fg=black> INFO </> '. 'Publishing [lx-views] assets.'. PHP_EOL .'   Copying directory [vendor/parth1895/lxbladecomponents/resources/views] to [resources/views/components/lx] .......................................... DONE' .PHP_EOL);
-        copy(__DIR__.'/../resources/views/components/*', app_path('resources/views/components/lx'));
+        (new Filesystem)->copyDirectory(__DIR__.'/../resources/views/components/', resource_path('resources/views/components/lx'));
+
     }
 
     public function __construct()
     {
         parent::__construct();
+    }
+
+    protected function runCommands($commands)
+    {
+        $process = Process::fromShellCommandline(implode(' && ', $commands), null, null, null, null);
+
+        if ('\\' !== DIRECTORY_SEPARATOR && file_exists('/dev/tty') && is_readable('/dev/tty')) {
+            try {
+                $process->setTty(true);
+            } catch (RuntimeException $e) {
+                $this->output->writeln('  <bg=yellow;fg=black> WARN </> '.$e->getMessage().PHP_EOL);
+            }
+        }
+
+        $process->run(function ($type, $line) {
+            $this->output->write('    '.$line);
+        });
     }
 }
