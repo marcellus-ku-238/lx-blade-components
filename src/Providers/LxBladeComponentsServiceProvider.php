b3750c54ee1561ef746f5516abdf84fdbf99caf3
Parth Gajjar <gajjar.parth1811@gmail.com>
2022-10-13 19:22:45 +0530

    [WIP]: Bug

--- src/Providers/LxBladeComponentsServiceProvider.php
+++ src/Providers/LxBladeComponentsServiceProvider.php
@@ -33,7 +33,7 @@ class LxBladeComponentsServiceProvider extends ServiceProvider
         }
 
         $this->publishes([
-            __DIR__.'/../resources/views' => resource_path('views/vendor/lx'),
+            __DIR__.'/../resources/views/components' => resource_path('views/components/lx'),
         ], 'lx-views');
     }
 
