1fd60b97ef0e4451d9876087ff9dfe5f8913e5e8
Parth Gajjar <gajjar.parth1811@gmail.com>
2022-10-13 20:55:31 +0530

    Improve component

--- src/resources/views/components/input.blade.php
+++ src/resources/views/components/input.blade.php
@@ -1,3 +1,12 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge([
+    'class' =>
+        'border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm',
+]) !!}>
+
+@if (Arr::has($attributes, 'wire:model'))
+    @error($attributes['wire:model'])
+        <span class="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">{{ $message }}</span>
+    @enderror
+@endif
