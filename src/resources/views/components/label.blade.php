ade9c67d8185f4c6040f138775e0057e3597d6c3
Parth Gajjar <gajjar.parth1811@gmail.com>
2022-10-13 22:04:30 +0530

    [IMP]: Update label component

--- src/resources/views/components/label.blade.php
+++ src/resources/views/components/label.blade.php
@@ -1,5 +1,8 @@
 @props(['value'])
 
-<label {{ $attributes->merge(['class' => (Arr::has($attributes, 'class')) ? $attributes['class'] : 'block text-gray-500 text-sm font-medium mb-2']) }}>
+<label
+    {{ $attributes->merge([
+        'class' => Arr::has($attributes, 'class') ? $attributes['class'] : 'block text-gray-600 text-sm font-medium mb-2',
+    ]) }}>
     {{ $value ?? $slot }}
 </label>
