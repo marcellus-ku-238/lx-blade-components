4ce8bd849200f18103a631d33432b0ba0874e62a
Parth Gajjar <gajjar.parth1811@gmail.com>
2022-10-13 17:09:27 +0530

    [WIP]: First blade testing

--- src/Providers/LxBladeComponentsServiceProvider.php
+++ src/Providers/LxBladeComponentsServiceProvider.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 namespace Parth1895\LxBladeComponents\Providers;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
+
 
 class LxBladeComponentsServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        $this->loadViewsFrom(__DIR__.'/../resources/views/components', 'components/lxb');
+        Blade::componentNamespace('Parth1895\\LxBladeComponents\\Resources\\Views\\Components\\Input', 'inputTest');
     }
 }
