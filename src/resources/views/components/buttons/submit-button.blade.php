82be770e21f1cf0f6e648040cfd685b6650d8bbf
Parth Gajjar <gajjar.parth1811@gmail.com>
2022-10-15 13:29:22 +0530

    Improve button component code

--- src/resources/views/components/buttons/submit-button.blade.php
+++ src/resources/views/components/buttons/submit-button.blade.php
@@ -1,22 +1,4 @@
 <div>
     {{ $submitBtn }}
     {{ $cancelBtn }}
-    <x-slot name="submit">
-        <button type="button" {!! $attributes->merge([
-            'class' =>
-                'inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium font-bold rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500',
-        ]) !!}>
-            {{ $slot }}
-        </button>
-
-    </x-slot>
-
-    <x-slot name="cancel">
-        <button type="button" {!! $attributes->merge([
-            'class' =>
-                'inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium font-bold rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500',
-        ]) !!}>
-            {{ $slot }}
-        </button>
-    </x-slot>
 </div>
--- src/resources/views/components/buttons/switch.blade.php
+++ src/resources/views/components/buttons/switch.blade.php
@@ -1,9 +1,14 @@
+@props([
+    'color' => 'indigo',
+])
+
+
 <div x-data='{
     open: false
 }'>
     <button x-on:click="open = !open" type="button" {!! $attributes->merge([
         'class' => 'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors
-                    ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 bg-indigo-600',
+                    ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-' . $color . '-500 bg-' . $color . '-600',
     ]) !!}
         :class="{ 'bg-slate-400 shadow-md ': open == true }">
         {{ $slot }}
