8924636d5da33babde6cbc2269c7ccf167878769
Parth Gajjar <gajjar.parth1811@gmail.com>
2022-10-15 13:09:38 +0530

    Updated swtich component for library

--- src/resources/views/components/buttons/switch.blade.php
+++ src/resources/views/components/buttons/switch.blade.php
@@ -1,11 +1,14 @@
 <div x-data='{
     open: false
 }'>
-    <button type="button" {!! $attributes->merge([
+    <button x-on:click="open = !open" type="button" {!! $attributes->merge([
         'class' => 'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors
-    ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 bg-indigo-600',
+                    ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 bg-indigo-600',
     ]) !!}
-        :class="{ 'bg-slate-400 transition shadow-md translate-x-7': open == true }">
+        :class="{ 'bg-slate-400 shadow-md ': open == true }">
         {{ $slot }}
+        <span :class="open ? 'transition translate-x-5' : 'transition translate-x-0'"
+            class="pointer-events-none h-5 w-5 rounded-full bg-white shadow transform ring-0 duration-200"
+            aria-hidden="true"></span>
     </button>
 </div>
