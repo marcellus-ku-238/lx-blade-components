6793af85b465e707e4cfe84918ea64ed7cd334e7
Parth Gajjar <gajjar.parth1811@gmail.com>
2022-10-15 12:21:14 +0530

    Fix: ui

--- src/resources/views/components/input.blade.php
+++ src/resources/views/components/input.blade.php
@@ -1,11 +1,11 @@
 @props(['disabled' => false])
 
 <input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge([
-    'class' => 'h-10 border border-gray-300 bg-gray-100 rounded-md focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm',
+    'class' => 'border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm text-sm bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5',
 ]) !!}>
 
 @if (Arr::has($attributes, 'wire:model'))
     @error($attributes['wire:model'])
-        <span class="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">{{ $message }}</span>
+        <span class="error text-sm font-medium text-red-700">{{ $message }}</span>
     @enderror
 @endif
