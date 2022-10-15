f97f033cf6a85b236b8791abd0389cba66403d2c
Parth Gajjar <gajjar.parth1811@gmail.com>
2022-10-15 15:59:17 +0530

    Update components

--- src/resources/views/components/action/banner.blade.php
+++ src/resources/views/components/action/banner.blade.php
@@ -1,46 +1,36 @@
-@props(['style' => session('flash.bannerStyle', 'success'), 'message' => session('flash.banner')])
+@props(['type' => 'success', 'autoHide' => false, 'message' => 'Operation completed successfully.'])
 
-<div x-data="{{ json_encode(['show' => true, 'style' => $style, 'message' => $message]) }}"
-            :class="{ 'bg-indigo-500': style == 'success', 'bg-red-700': style == 'danger', 'bg-gray-500': style != 'success' && style != 'danger' }"
-            style="display: none;"
-            x-show="show && message"
-            x-init="
-                document.addEventListener('banner-message', event => {
-                    style = event.detail.style;
-                    message = event.detail.message;
-                    show = true;
-                });
-            ">
-    <div class="max-w-screen-xl mx-auto py-2 px-3 sm:px-6 lg:px-8">
-        <div class="flex items-center justify-between flex-wrap">
-            <div class="w-0 flex-1 flex items-center min-w-0">
-                <span class="flex p-2 rounded-lg" :class="{ 'bg-indigo-600': style == 'success', 'bg-red-600': style == 'danger' }">
-                    <svg x-show="style == 'success'" class="h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                    </svg>
-                    <svg x-show="style == 'danger'" class="h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                    </svg>
-                    <svg x-show="style != 'success' && style != 'danger'" class="h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                    </svg>
-                </span>
+<div x-data="{
+    open: true,
+    autoHide: '{{ $autoHide }}',
+    init() {
+        if (this.autoHide == true) {
+            setTimeout(() => this.open = false, 1200)
+        }
+    }
+}">
+    <div @class([
+        'text-green-400 bg-green-100' => $type == 'success',
+        'text-red-400 bg-red-100' => $type == 'failed',
+        'text-yellow-400 bg-yellow-100' => $type == 'warning',
+        'text-blue-400 bg-blue-100' => $type == 'info',
+        'max-w-screen-xl mx-auto p-3 sm:px-6 lg:px-8 rounded-md flex place-content-between',
+    ]) x-show="open">
+        <p class="font-black text-sm truncate">
+            {{ $slot->isEmpty() ? $message : $slot }}
+        </p>
 
-                <p class="ml-3 font-medium text-sm text-white truncate" x-text="message"></p>
-            </div>
-
-            <div class="shrink-0 sm:ml-3">
-                <button
-                    type="button"
-                    class="-mr-1 flex p-2 rounded-md focus:outline-none sm:-mr-2 transition"
-                    :class="{ 'hover:bg-indigo-600 focus:bg-indigo-600': style == 'success', 'hover:bg-red-600 focus:bg-red-600': style == 'danger' }"
-                    aria-label="Dismiss"
-                    x-on:click="show = false">
-                    <svg class="h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
-            </div>
-        </div>
+        <button type="button" x-on:click="open = false">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="w-6 h-6" @class([
+                    'text-green-400' => $type == 'success',
+                    'text-red-400' => $type == 'failed',
+                    'text-yellow-400' => $type == 'warning',
+                    'text-blue-400' => $type == 'info',
+                    'w-5 h-5 font-black text-sm',
+                ])>
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+        </button>
     </div>
 </div>
