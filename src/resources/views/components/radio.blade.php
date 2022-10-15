c4d7f05dcadf4320c341b38d63495ac4497747cd
Parth Gajjar <gajjar.parth1811@gmail.com>
2022-10-15 12:31:54 +0530

    Added radio to library

--- src/resources/views/components/radio.blade.php
+++ src/resources/views/components/radio.blade.php
@@ -1,8 +1,11 @@
 @props(['disabled' => false])
 
-<input type="radio" {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge([
-    'class' => 'rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-primary-500 border-2 border-primary-500 focus:ring-primary-500 h-5 w-5 rounded-full',
-]) !!}>
+<div class="flex space-x-2">
+    <input type="radio" {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge([
+        'class' =>
+            'rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-primary-500 border-2 border-primary-500 focus:ring-primary-500 h-5 w-5 rounded-full',
+    ]) !!}>
+</div>
 
 @if (Arr::has($attributes, 'wire:model'))
     @error($attributes['wire:model'])
