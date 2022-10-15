a7810b8656777cd2f1ee98ebfa48f13ee129a933
Parth Gajjar <gajjar.parth1811@gmail.com>
2022-10-15 12:39:44 +0530

    Added dropdown in library

---
+++ src/resources/views/components/select.blade.php
@@ -0,0 +1,14 @@
+@props(['disabled' => false])
+
+<select {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge([
+    'class' =>
+        'h-10 border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm text-sm bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5',
+]) !!}>
+    {{ $slot }}
+</select>
+
+@if (Arr::has($attributes, 'wire:model'))
+    @error($attributes['wire:model'])
+        <span class="error text-sm font-medium text-red-700">{{ $message }}</span>
+    @enderror
+@endif
