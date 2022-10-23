2487046938d106c33ed7662f5163fd9a0d0d81a1
Parth Gajjar <parth@selcom.net>
2022-10-23 17:09:56 +0530

    Update component naming

--- src/Providers/LxBladeComponentsServiceProvider.php
+++ src/Providers/LxBladeComponentsServiceProvider.php
@@ -41,7 +41,7 @@ class LxBladeComponentsServiceProvider extends ServiceProvider
         }
 
         $this->publishes([
-            __DIR__.'/../resources/views/components' => resource_path('views/components/lx'),
+            __DIR__.'/../resources/views/components' => resource_path('views/components/'),
         ], 'lx-views');
     }
 
