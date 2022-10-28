43e3b34848e288da7844df00ec95e6cea9bbb08c
Parth Gajjar <parth@selcom.net>
2022-10-28 22:20:57 +0530

    Update navlinks

--- src/resources/views/components/nav-link.blade.php
+++ src/resources/views/components/nav-link.blade.php
@@ -1,7 +1,7 @@
-@props(['active'])
+@props(['active', 'color' => 'indigo'])
 
 @php
-    $classes = $active ?? false ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition' : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition';
+    $classes = $active ?? false ? 'inline-flex items-center px-1 pt-1 border-b-2 border-' . $color . '-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-' . $color . '-700 transition' : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
--- src/resources/views/components/responsive-nav-link.blade.php
+++ src/resources/views/components/responsive-nav-link.blade.php
@@ -1,7 +1,7 @@
-@props(['active'])
+@props(['active', 'color' => 'indigo'])
 
 @php
-    $classes = $active ?? false ? 'block pl-3 pr-4 py-2 border-l-4 border-indigo-400 text-base font-medium text-indigo-700 bg-indigo-50 focus:outline-none focus:text-indigo-800 focus:bg-indigo-100 focus:border-indigo-700 transition' : 'block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition';
+    $classes = $active ?? false ? 'block pl-3 pr-4 py-2 border-l-4 border-' . $color . '-400 text-base font-medium text-' . $color . '-700 bg-' . $color . '-50 focus:outline-none focus:text-' . $color . '-800 focus:bg-' . $color . '-100 focus:border-' . $color . '-700 transition' : 'block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
